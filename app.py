606d776babcb28218b1d546c560ee156829ac88e
tiffanyledge <tiffany.lee@csedge.org>
2023-02-18 15:57:28 -0500

    partically completed web

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    response = requests.get(f'https://ws-public.interpol.int/notices/v1/red?ageMax=50&ageMin=18&resultPerPage=10')
+    response = requests.get(f'https://ws-public.interpol.int/notices/v1/red?ageMax=50&ageMin=18&resultPerPage=60')
     data = response.json()
     # print(data)
     criminals = []
@@ -14,21 +14,28 @@ def index():
     #   charges: '',
     #   pic: ''
     # }
-
-    for x in range(10):
+    entity_id = []
+    
+    for x in range(60):
         #print(data['_embedded']['notices'][x]['forename'])
         criminal = { 
+            'pic': '',
             'name': '',
-            'charges': '',
-            'pic': ''
+            'charges': ''
         }
         criminal['name'] = data['_embedded']['notices'][x]['forename']
         #print(data['_links']['self']['href'])
         response = requests.get(data['_embedded']['notices'][x]['_links']['self']['href'])
         #print(response.json())
         criminal['charges'] = response.json()['arrest_warrants'][0]['charge']
+        response1 = requests.get(data['_embedded']['notices'][x]['_links']['thumbnail']['href'])
+        
+        criminal['pic'] = data['_embedded']['notices'][x]['entity_id']
+        criminal['id'] = data['_embedded']['notices'][x]['_links']['thumbnail']['href']
+        criminal['dob'] = data['_embedded']['notices'][x]['date_of_birth']
         criminals.append(criminal)
-        # print(criminals)
+
+        print(criminals)
 
     
         # print(data['_link']['self'])
